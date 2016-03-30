ab0ad672fdf5d23316b6bb0fb9033d71ea4a024a
sidikhmenova <esidikhmenova@gmail.com>
2016-03-30 20:48:33 +1000

    add ===

--- src/check.js
+++ src/check.js
@@ -6,7 +6,7 @@ function getMessage (a, b) {
   var result;
 
   //если первый аргумент логический тип
-  if (typeof a == 'boolean') {
+  if (typeof a === 'boolean') {
     if (a) {
       result = 'Я попал в ' + b;
     }
@@ -16,12 +16,12 @@ function getMessage (a, b) {
   }
 
   //если числовой тип
-  else if (typeof a == 'number') {
+  else if (typeof a === 'number') {
     result = 'Я прыгнул на ' + a*100 + ' сантиметров';
   }
 
   //если оба массивы
-  else if (typeof a == 'object' && typeof b == 'object') {
+  else if (typeof a === 'object' && typeof b === 'object') {
     var length = 0;
     for (var i = 0; i < a.length; i++) {
       if (i < b.length) {
@@ -32,7 +32,7 @@ function getMessage (a, b) {
   }
 
   //если массив только а
-  else if (typeof a == 'object') {
+  else if (typeof a === 'object') {
     var sum = 0;
     for (var i = 0; i < a.length; i++) {
       sum += a[i];
