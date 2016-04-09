ae87d76a46718cf57182ceedb8e0e84faec9d2cd
sidikhmenova <esidikhmenova@gmail.com>
2016-04-10 01:29:33 +1000

    add AJAX and filtered reviews

--- src/reviews.js
+++ src/reviews.js
@@ -5,12 +5,16 @@
 'use strict';
 
 (function() {
-  var filter = document.querySelector('.reviews-filter');
+  var filterList = document.querySelector('.reviews-filter');
+  var filterItem = filterList['reviews'];
   var reviewContainer = document.querySelector('.reviews-list');
   var template = document.querySelector('#review-template');
+  var sectionReviews = document.querySelector('.reviews');
   var cloneElement;
+  var reviews;
+  var activeFilter = 'reviews-all';
 
-  filter.classList.add('invisible');
+  filterList.classList.add('invisible');
 
 // 1 Находим нужный элемент в разметке. Для поддержки старыми браузерами (усли браузер не поддерживает тег template) добавляем else
   if ('content' in template) {
@@ -21,6 +25,8 @@
 
   /** @constant {number} */
   var IMAGE_LOAD_TIMEOUT = 10000;
+
+  var REVIEW_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
 
 // 2
   /**
@@ -36,11 +42,9 @@
     // вставлеяем элемент в DOM
     container.appendChild(element);
 
-    var reviewAutor = element.querySelector('.review-author');
-    reviewAutor.alt = 'Отзыв пользоватедя ' + data.author.name + ' об игре Code & Magick';
-    reviewAutor.title = data.name;
-
     var backgroundImage = new Image();
+    backgroundImage.alt = 'Отзыв пользоватедя ' + data.author.name + ' об игре Code & Magick';
+    backgroundImage.title = data.author.name;
     var backgroundLoadTimeout;
 
     backgroundImage.addEventListener('load', function() {
@@ -67,18 +71,117 @@
     var reviewRatingBlock = element.querySelector('.review-rating');
     var rating = data.rating;
 
-    var transRating = ['one', 'two', 'tree', 'four', 'five'];
+    var transRating = ['one', 'two', 'three', 'four', 'five'];
 
     reviewRatingBlock.classList.add('review-rating-' + transRating[rating - 1]);
 
     return element;
   };
 
-  window.reviews.forEach(function(reviewItem) {
-    getReviewElement(reviewItem, reviewContainer);
+  /** @param {function(Array.<Object>)} callback */
+  var getReviewList = function(callback) {
+    var xhr = new XMLHttpRequest();
+
+    /** @param {ProgressEvent} */
+    xhr.onload = function(evt) {
+      sectionReviews.classList.add('reviews-list-loading');
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+      sectionReviews.classList.remove('reviews-list-loading');
+    };
+
+    xhr.onerror = function() {
+      reviewsFailure();
+    };
+
+    xhr.timeout = IMAGE_LOAD_TIMEOUT;
+    xhr.ontimeout = function() {
+      reviewsFailure();
+    };
+
+    xhr.open('GET', REVIEW_LOAD_URL);
+    xhr.send();
+  };
+
+  // Функция, возвращающая ошибку
+  function reviewsFailure() {
+    sectionReviews.classList.remove('reviews-list-loading');
+    sectionReviews.classList.add('reviews-load-failure');
+  }
+
+  /** @param {Array.<Object>} reviews */
+  var renderReview = function(reviews) {
+    reviewContainer.innerHTML = '';
+
+    reviews.forEach(function(reviewItem) {
+      getReviewElement(reviewItem, reviewContainer);
+    });
+  };
+
+  // Обработчик клика на блок с оценками
+  filterList.addEventListener('change', function() {
+    console.log(filterItem.value);
+    setActiveFilter(filterItem.value);
   });
 
-  filter.classList.remove('invisible');
+  function setActiveFilter(id) {
+    if (activeFilter === id) {
+      return;
+    }
+
+    var filteredReviews = reviews.slice(0);
+    console.log(reviews);
+    switch (id) {
+      case 'reviews-recent':
+        // показывает список отзывов, оставленных за две недели, отсортированных по убыванию даты
+        filteredReviews = filteredReviews.sort(function(a, b) {
+          return b.date - a.date;
+        });
+        //// фильтруем массив с датами и отбираем изображения за 2 недели
+        //filteredReviews = filteredReviews.filter(selectedDay);
+        break;
+      case 'reviews-good':
+        //  с рейтингом не ниже 3, отсортированные по убыванию рейтинга
+        filteredReviews = filteredReviews.sort(function(a, b) {
+          return b.rating - a.rating;
+        });
+        break;
+      case 'reviews-bad':
+        //  с рейтингом не выше 2, отсортированные по возрастанию рейтинга
+        filteredReviews = filteredReviews.sort(function(a, b) {
+          return a.rating - b.rating;
+        });
+        break;
+      case 'reviews-popular':
+        //  отсортированные по убыванию оценки отзыва
+        filteredReviews = filteredReviews.sort(function(a, b) {
+          return b.review_usefulness - a.review_usefulness;
+        });
+        break;
+    }
+
+    renderReview(filteredReviews);
+  }
+
+  //var lastDate = new Date();
+  //var lastMonth = lastDate.getMonth();
+  //lastMonth = lastMonth - 3;
+  //lastDate.setMonth(lastMonth);
+  //
+  //// Функция анализа даты публикации
+  //function selectedDay(date) {
+  //  // делаем выборку за последние 3 месяца
+  //  var pictureDate = new Date(date.date);
+  //  return pictureDate > lastDate;
+  //}
+
+  getReviewList(function(loadedReviews) {
+    reviews = loadedReviews;
+    console.log(reviews);
+    renderReview(reviews);
+  });
+
+  filterList.classList.remove('invisible');
 })();
 
 
