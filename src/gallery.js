4e9929fd1bcf14318bf9e988984f4505b0f830b1
sidikhmenova <esidikhmenova@gmail.com>
2016-04-27 22:32:27 +1000

    add object gallery

--- src/gallery.js
+++ src/gallery.js
@@ -20,80 +20,95 @@ var btnClose = document.querySelector('.overlay-gallery-close');
 var spanCurrent = document.querySelector('.preview-number-current');
 var spanTotal = document.querySelector('.preview-number-total');
 
-/** @type {Array.<string>} */
-var galleryPicture = [];
 var currentNum = 1;
 
-getPhotoGallery(photos);
+function Gallery() {
+  var self = this;
 
-function getPhotoGallery(pct) {
-  for (var i = 0; i < pct.length; i++) {
-    galleryPicture[i] = pct[i].src;
-  }
-}
+  this.galleryPicture = [];
 
-function showGallery() {
-  galleryContainer.classList.remove('invisible');
-  spanTotal.textContent = galleryPicture.length;
-
-  element = new Image();
-  mainPhoto = preview.appendChild(element);
-
-  showActivePhoto();
-}
-
-function showActivePhoto() {
-  mainPhoto.src = galleryPicture[currentNum];
-  spanCurrent.textContent = currentNum + 1;
-  visibleButton();
-}
-
-function getActivePhoto(clkElement) {
-  for (var i = 0; i < galleryPicture.length; i++) {
-    if (galleryPicture[i] === clkElement) {
-      return i;
+  this.getPhotoGallery = function(pct) {
+    for (var i = 0; i < pct.length; i++) {
+      self.galleryPicture[i] = pct[i].src;
     }
-  }
-  return null;
+  };
+
+  this.initialClick = function(evt) {
+    evt.preventDefault();
+    clickedElement = evt.target.src;
+    currentNum = self.getActivePhoto(clickedElement);
+    self.showGallery();
+  };
+
+  this.getActivePhoto = function(clkElement) {
+    for (var i = 0; i < self.galleryPicture.length; i++) {
+      if (self.galleryPicture[i] === clkElement) {
+        return i;
+      }
+    }
+    return null;
+  };
+
+  this.showGallery = function() {
+    galleryContainer.classList.remove('invisible');
+    spanTotal.textContent = self.galleryPicture.length;
+
+    element = new Image();
+    mainPhoto = preview.appendChild(element);
+
+    btnNext.addEventListener('click', self.showNextPage);
+    btnBefore.addEventListener('click', self.showBeforePage);
+    btnClose.addEventListener('click', self.onCloseClickGallery);
+    window.addEventListener('keydown', self.onCloseKeydownGallery);
+
+    self.showActivePhoto();
+  };
+
+  this.showActivePhoto = function() {
+    mainPhoto.src = self.galleryPicture[currentNum];
+    spanCurrent.textContent = currentNum + 1;
+    self.visibleButton();
+  };
+
+  this.visibleButton = function() {
+    btnBefore.classList.toggle('invisible', currentNum === 0);
+    btnNext.classList.toggle('invisible', (currentNum + 1) === self.galleryPicture.length);
+  };
+
+  this.closeGallery = function() {
+    mainPhoto = preview.removeChild(element);
+
+    btnNext.removeEventListener('click', self.showNextPage);
+    btnBefore.removeEventListener('click', self.showBeforePage);
+    btnClose.removeEventListener('click', self.onCloseClickGallery);
+    window.removeEventListener('keydown', self.onCloseKeydownGallery);
+
+    galleryContainer.classList.add('invisible');
+  };
+
+  this.showNextPage = function() {
+    ++currentNum;
+    self.showActivePhoto(currentNum);
+  };
+
+  this.showBeforePage = function() {
+    --currentNum;
+    self.showActivePhoto(currentNum);
+  };
+
+  this.onCloseClickGallery = function() {
+    self.closeGallery();
+  };
+
+  this.onCloseKeydownGallery = function(evt) {
+    if (evt.keyCode === 27) {
+      self.closeGallery();
+    }
+  };
+
+  this.element = this.getPhotoGallery(photos);
+
+  photoGalleryContainer.addEventListener('click', self.initialClick);
 }
 
-function visibleButton() {
-  btnBefore.classList.toggle('invisible', currentNum === 0);
-  btnNext.classList.toggle('invisible', (currentNum + 1) === galleryPicture.length);
-}
-
-function closeGallery() {
-  mainPhoto = preview.removeChild(element);
-  galleryContainer.classList.add('invisible');
-}
-
-photoGalleryContainer.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  clickedElement = evt.target.src;
-  currentNum = getActivePhoto(clickedElement);
-  showGallery();
-});
-
-btnNext.addEventListener('click', function() {
-  ++currentNum;
-  showActivePhoto(currentNum);
-});
-
-btnBefore.addEventListener('click', function() {
-  --currentNum;
-  showActivePhoto(currentNum);
-});
-
-btnClose.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  closeGallery();
-});
-
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    closeGallery();
-  }
-});
-
-module.exports = getPhotoGallery;
-module.exports = showGallery;
+module.exports = new Gallery();
