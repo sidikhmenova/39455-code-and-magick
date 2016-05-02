7d5c35ba63e1735e9f386caf4b0b1dd3ffdf6b93
sidikhmenova <esidikhmenova@gmail.com>
2016-05-02 15:55:47 +1000

    add prototype

--- src/gallery.js
+++ src/gallery.js
@@ -3,207 +3,187 @@
  */
 
 'use strict';
+var galleryContainer = document.querySelector('.overlay-gallery');
+var preview = document.querySelector('.overlay-gallery-preview');
+
+var photoGalleryContainer = document.querySelector('.photogallery');
+var photos = document.querySelectorAll('.photogallery img');
+
+var btnNext = document.querySelector('.overlay-gallery-control-right');
+var btnBefore = document.querySelector('.overlay-gallery-control-left');
+var btnClose = document.querySelector('.overlay-gallery-close');
+
+var spanCurrent = document.querySelector('.preview-number-current');
+var spanTotal = document.querySelector('.preview-number-total');
 
 /** @constructor */
 function Gallery() {
-  this.galleryContainer = document.querySelector('.overlay-gallery');
-  this.preview = document.querySelector('.overlay-gallery-preview');
-
-  this.photoGalleryContainer = document.querySelector('.photogallery');
-  this.photos = document.querySelectorAll('.photogallery img');
-
   this.clickedElement = null;
   this.mainPhoto = null;
   this.element = null;
   this.isShowGallery = null;
+  this.currentNum = 1;
+  this.galleryPicture = [];
 
   this.REG_STRING = /#photo\/(\S+)/;
 
-  this.btnNext = document.querySelector('.overlay-gallery-control-right');
-  this.btnBefore = document.querySelector('.overlay-gallery-control-left');
-  this.btnClose = document.querySelector('.overlay-gallery-close');
+  this.getPhotoGallery = this.getPhotoGallery.bind(this);
+  this.initialClick = this.initialClick.bind(this);
+  this.getActivePhoto = this.getActivePhoto.bind(this);
+  this.hashChange = this.hashChange.bind(this);
+  this.showGallery = this.showGallery.bind(this);
+  this.showActivePhoto = this.showActivePhoto.bind(this);
+  this.visibleButton = this.visibleButton.bind(this);
+  this.closeGallery = this.closeGallery.bind(this);
+  this.visibleButton = this.visibleButton.bind(this);
+  this.showNextPage = this.showNextPage.bind(this);
+  this.showBeforePage = this.showBeforePage.bind(this);
+  this.clearHash = this.clearHash.bind(this);
+  this.onCloseKeydownGallery = this.onCloseKeydownGallery.bind(this);
 
-  this.spanCurrent = document.querySelector('.preview-number-current');
-  this.spanTotal = document.querySelector('.preview-number-total');
-
-  this.currentNum = 1;
-
-  var self = this;
-
-  this.galleryPicture = [];
-
-  /**
-   * Функция получения массива фотографий, на основе фотографий, расположенных на главной странице
-   * @param {Array} pct
-   */
-  this.getPhotoGallery = function(pct) {
-    for (var i = 0; i < pct.length; i++) {
-      this.galleryPicture[i] = pct[i].getAttribute('src');
-    }
-  };
-
-  /**
-   * Функция первоначального клика на фотографии с главной страницы
-   * @param {KeyboardEvent} evt
-   */
-  this.initialClick = function(evt) {
-    evt.preventDefault();
-    this.clickedElement = evt.target.getAttribute('src');
-    this.currentNum = self.getActivePhoto(this.clickedElement);
-    location.hash = '#photo/' + self.galleryPicture[this.currentNum];
-  };
-
-  /**
-   * Функция определения индекса в массиве по переданному элементу. Используется во всех случаях изменения фото.
-   * @param {string} clkElement
-   * @returns {number}
-   */
-  this.getActivePhoto = function(clkElement) {
-    for (var i = 0; i < self.galleryPicture.length; i++) {
-      if (self.galleryPicture[i] === clkElement) {
-        return i;
-      }
-    }
-    return null;
-  };
-
-  /**
-   * Функция анализа изменения значений в адресной строке
-   */
-  this.hashChange = function() {
-    var galleryHash = window.location.href.match(self.REG_STRING);
-
-    if (galleryHash && self.isShowGallery) {
-      self.showActivePhoto();
-    } else if(galleryHash && !self.isShowGallery) {
-      self.showGallery();
-    } else if(!galleryHash && self.isShowGallery) {
-      self.closeGallery();
-    }
-  };
-
-  /**
-   * Функция отрисовки галереи
-   */
-  this.showGallery = function() {
-    this.isShowGallery = true;
-
-    this.galleryContainer.classList.remove('invisible');
-    this.spanTotal.textContent = this.galleryPicture.length;
-
-    this.element = new Image();
-    this.mainPhoto = this.preview.appendChild(this.element);
-
-    this.btnNext.addEventListener('click', function() {
-      self.showNextPage();
-    });
-    this.btnBefore.addEventListener('click', function() {
-      self.showBeforePage();
-    });
-    this.btnClose.addEventListener('click', function() {
-      self.onCloseClickGallery();
-    });
-    window.addEventListener('keydown', function() {
-      self.onCloseKeydownGallery();
-    });
-
-    self.showActivePhoto();
-  };
-
-  /**
-   * Функция отрисовки активной фото
-   */
-  this.showActivePhoto = function() {
-    this.currentNum = this.getActivePhoto(window.location.href.match(this.REG_STRING)[1]);
-
-    if (this.currentNum) {
-      this.mainPhoto.src = this.galleryPicture[this.currentNum];
-      this.spanCurrent.textContent = this.currentNum + 1;
-      this.visibleButton();
-    } else {
-      location.hash = '';
-    }
-  };
-
-  /**
-   * Функция, отвечающая за отображение кнопок "вперед", "назад". Анализирует значение текущей фотографии по отношению к макс, мин индексу массива
-   */
-  this.visibleButton = function() {
-    this.btnBefore.classList.toggle('invisible', self.currentNum === 0);
-    this.btnNext.classList.toggle('invisible', (self.currentNum + 1) === self.galleryPicture.length);
-  };
-
-  /**
-   * Функция закрытия блока галлереи
-   */
-  this.closeGallery = function() {
-    this.isShowGallery = false;
-    this.mainPhoto = this.preview.removeChild(this.element);
-
-    this.btnNext.removeEventListener('click', function() {
-      self.showNextPage();
-    });
-    this.btnBefore.removeEventListener('click', function() {
-      self.showBeforePage();
-    });
-    this.btnClose.removeEventListener('click', function() {
-      self.onCloseClickGallery();
-    });
-    window.removeEventListener('keydown', function() {
-      self.onCloseKeydownGallery();
-    });
-
-    this.galleryContainer.classList.add('invisible');
-  };
-
-  /**
-   * Функция, отвечающая за отображение след.страницы. Передает в функцию редактирования hash строки - на сколько нужно изменить тек.позицию в массиве
-   */
-  this.showNextPage = function() {
-    self.editLocationHash(1);
-  };
-
-  /**
-   * Функция, отвечающая за отображение пред.страницы. Передает в функцию редактирования hash строки - на сколько нужно изменить тек.позицию в массиве
-   */
-  this.showBeforePage = function() {
-    self.editLocationHash(-1);
-  };
-
-  /**
-   * Функция, отвечающая за редактирования hash строки на основе переданных данных. Вызывается при листании "вперед", "назад"
-   * @param {number} num
-   */
-  this.editLocationHash = function(num) {
-    this.currentNum = this.currentNum + num;
-    location.hash = '#photo/' + self.galleryPicture[self.currentNum];
-  };
-
-  /**
-   * Функция, измененяющая значение hash при иницииации закрытия формы
-   */
-  this.onCloseClickGallery = function() {
-    location.hash = '';
-  };
-
-  /**
-   * @param {KeyboardEvent} evt
-   */
-  this.onCloseKeydownGallery = function(evt) {
-    if (evt.keyCode === 27) {
-      location.hash = '';
-    }
-  };
-
-  this.element = this.getPhotoGallery(this.photos);
+  this.element = this.getPhotoGallery();
   this.hashChange();
 
-  this.photoGalleryContainer.addEventListener('click', function(evt) {
-    self.initialClick(evt);
-  });
-  window.addEventListener('hashchange', function() {
-    self.hashChange();
-  });
+  photoGalleryContainer.addEventListener('click', this.initialClick);
+  window.addEventListener('hashchange', this.hashChange.bind(this));
 }
+
+/**
+ * Функция получения массива фотографий, на основе фотографий, расположенных на главной странице
+ */
+Gallery.prototype.getPhotoGallery = function() {
+  for (var i = 0; i < photos.length; i++) {
+    this.galleryPicture[i] = photos[i].getAttribute('src');
+  }
+};
+
+/**
+ * Функция первоначального клика на фотографии с главной страницы
+ * @param {KeyboardEvent} evt
+ */
+Gallery.prototype.initialClick = function(evt) {
+  evt.preventDefault();
+  this.clickedElement = evt.target.getAttribute('src');
+  this.currentNum = this.getActivePhoto(this.clickedElement);
+  location.hash = '#photo/' + this.galleryPicture[this.currentNum];
+};
+
+/**
+ * Функция определения индекса в массиве по переданному элементу. Используется во всех случаях изменения фото.
+ * @param {string} clkElement
+ * @returns {number}
+ */
+Gallery.prototype.getActivePhoto = function(clkElement) {
+  for (var i = 0; i < this.galleryPicture.length; i++) {
+    if (this.galleryPicture[i] === clkElement) {
+      return i;
+    }
+  }
+  return null;
+};
+
+/**
+ * Функция анализа изменения значений в адресной строке
+ */
+Gallery.prototype.hashChange = function() {
+  var galleryHash = window.location.href.match(this.REG_STRING);
+
+  if (galleryHash && this.isShowGallery) {
+    this.showActivePhoto();
+  } else if(galleryHash && !this.isShowGallery) {
+    this.showGallery();
+  } else if(!galleryHash && this.isShowGallery) {
+    this.closeGallery();
+  }
+};
+
+/**
+ * Функция отрисовки галереи
+ */
+Gallery.prototype.showGallery = function() {
+  this.isShowGallery = true;
+
+  galleryContainer.classList.remove('invisible');
+  spanTotal.textContent = this.galleryPicture.length;
+
+  this.element = new Image();
+  this.mainPhoto = preview.appendChild(this.element);
+
+  btnNext.addEventListener('click', this.showNextPage);
+  btnBefore.addEventListener('click', this.showBeforePage);
+  btnClose.addEventListener('click', this.clearHash);
+  window.addEventListener('keydown', this.onCloseKeydownGallery);
+
+  this.showActivePhoto();
+};
+
+/**
+ * Функция отрисовки активной фото
+ */
+Gallery.prototype.showActivePhoto = function() {
+  this.currentNum = this.getActivePhoto(window.location.href.match(this.REG_STRING)[1]);
+
+  if (this.currentNum >= 0) {
+    this.mainPhoto.src = this.galleryPicture[this.currentNum];
+    spanCurrent.textContent = this.currentNum + 1;
+    this.visibleButton();
+  } else {
+    this.clearHash();
+  }
+};
+
+/**
+ * Функция, отвечающая за отображение кнопок "вперед", "назад". Анализирует значение текущей фотографии по отношению к макс, мин индексу массива
+ */
+Gallery.prototype.visibleButton = function() {
+  btnBefore.classList.toggle('invisible', this.currentNum === 0);
+  btnNext.classList.toggle('invisible', (this.currentNum + 1) === this.galleryPicture.length);
+};
+
+/**
+ * Функция, меняющая адрес в hash для отображения след.картинки
+ */
+Gallery.prototype.showNextPage = function() {
+  location.hash = '#photo/' + this.galleryPicture[this.currentNum + 1];
+};
+
+/**
+ * Функция, меняющая адрес в hash для отображения пред.картинки
+ */
+Gallery.prototype.showBeforePage = function() {
+  location.hash = '#photo/' + this.galleryPicture[this.currentNum - 1];
+};
+
+/**
+ * Функция закрытия блока галлереи
+ */
+Gallery.prototype.closeGallery = function() {
+  this.isShowGallery = false;
+  this.mainPhoto = preview.removeChild(this.element);
+
+  btnNext.removeEventListener('click', this.showNextPage);
+  btnBefore.removeEventListener('click', this.showBeforePage);
+  btnClose.removeEventListener('click', this.clearHash);
+  window.removeEventListener('keydown', this.onCloseKeydownGallery);
+
+  galleryContainer.classList.add('invisible');
+};
+
+/**
+ * Функция, отвечающая за редактирования hash строки на основе переданных данных. Вызывается при листании "вперед", "назад"
+ */
+Gallery.prototype.clearHash = function() {
+  location.hash = '';
+};
+
+/**
+ * Функция, выполняющая анализ нажатой клавиши
+ * @param {KeyboardEvent} evt
+ */
+Gallery.prototype.onCloseKeydownGallery = function(evt) {
+  if (evt.keyCode === 27) {
+    this.clearHash();
+  }
+};
 
 module.exports = new Gallery();
--- src/reviews/review.js
+++ src/reviews/review.js
@@ -23,62 +23,72 @@ if ('content' in template) {
 function Review(data, container) {
   this.data = data;
 
-  this.getReview = function() {
-    // Клонируем шаблонный элемент и заполняем элементы отеля данными из объекта data
-    var element = cloneElement.cloneNode(true);
-
-    var backgroundImage = new Image();
-    var backgroundLoadTimeout;
-
-    var reviewRatingBlock = element.querySelector('.review-rating');
-    var rating = data.rating;
-    var transRating = ['one', 'two', 'three', 'four', 'five'];
-
-    element.querySelector('.review-text').textContent = data.description;
-
-    // вставлеяем элемент в DOM
-    container.appendChild(element);
-
-    backgroundImage.addEventListener('load', function() {
-      clearTimeout(backgroundLoadTimeout);
-      element.replaceChild(backgroundImage, element.children[0]);
-      backgroundImage.classList.add('review-author');
-      backgroundImage.alt = 'Отзыв пользователя ' + data.author.name + ' об игре Code & Magick';
-      backgroundImage.title = data.author.name;
-      backgroundImage.width = 124;
-      backgroundImage.height = 124;
-    });
-
-    backgroundImage.addEventListener('error', function() {
-      element.classList.add('review-load-failure');
-    });
-
-    backgroundImage.src = data.author.picture;
-
-    backgroundLoadTimeout = setTimeout(function() {
-      backgroundImage.src = '';
-      element.classList.add('review-load-failure');
-    }, IMAGE_LOAD_TIMEOUT);
-
-    reviewRatingBlock.classList.add('review-rating-' + transRating[rating - 1]);
-
-    return element;
-  };
-
   this.element = this.getReview();
   this.element.addEventListener('click', this.onClickRQuizAnswer);
 
-  this.onClickRQuizAnswer = function(evt) {
-    if (evt.target.classList.contains(quizAnswer)) {
-      evt.preventDefault();
-      evt.target.classList.add(quizAnswerActive);
-    }
-  };
-
-  this.remove = function() {
-    this.element.removeEventListener('click', this.onClickRQuizAnswer);
-    this.element.parentNode.removeChild(this.element);
-  };
+  container.appendChild(this.element);
 }
+
+/**
+ * Функция работы с полученным отзывом по шаблону.
+ * @returns {Node}
+ */
+Review.prototype.getReview = function() {
+  // Клонируем шаблонный элемент и заполняем элементы отеля данными из объекта data
+  var element = cloneElement.cloneNode(true);
+
+  var backgroundImage = new Image();
+  var backgroundLoadTimeout;
+
+  var reviewRatingBlock = element.querySelector('.review-rating');
+  var rating = this.data.rating;
+  var transRating = ['one', 'two', 'three', 'four', 'five'];
+
+  element.querySelector('.review-text').textContent = this.data.description;
+
+  backgroundImage.addEventListener('load', function() {
+    clearTimeout(backgroundLoadTimeout);
+    element.replaceChild(backgroundImage, element.children[0]);
+    backgroundImage.classList.add('review-author');
+    backgroundImage.alt = 'Отзыв пользователя ' + this.data.author.name + ' об игре Code & Magick';
+    backgroundImage.title = this.data.author.name;
+    backgroundImage.width = 124;
+    backgroundImage.height = 124;
+  });
+
+  backgroundImage.addEventListener('error', function() {
+    element.classList.add('review-load-failure');
+  });
+
+  backgroundImage.src = this.data.author.picture;
+
+  backgroundLoadTimeout = setTimeout(function() {
+    backgroundImage.src = '';
+    element.classList.add('review-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+
+  reviewRatingBlock.classList.add('review-rating-' + transRating[rating - 1]);
+
+  return element;
+};
+
+/**
+ * Функция обработчика события клика на блок "Полезный отзыв"
+ * @param evt
+ */
+Review.prototype.onClickRQuizAnswer = function(evt) {
+  if (evt.target.classList.contains(quizAnswer)) {
+    evt.preventDefault();
+    evt.target.classList.add(quizAnswerActive);
+  }
+};
+
+/**
+ * Функция очистки блока с отзывами
+ */
+Review.prototype.remove = function() {
+  this.element.removeEventListener('click', this.onClickRQuizAnswer);
+  this.element.parentNode.removeChild(this.element);
+};
 
 module.exports = Review;
